d4da4155f08983056aef5a98fac249b8f34746ba
Manmeet567 <manmeet567singh@gmail.com>
2025-03-14 14:58:16 +0530

    Fix: Fixed entry point in config file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
     apps: [
       {
         name: "nap-works-assignment", // The name of the application
-        script: "./server.js", // The entry point (Change if different)
+        script: "server.js", // The entry point (Change if different)
         instances: "max", // Uses all available CPU cores
         exec_mode: "cluster", // Enables load balancing
         watch: false, // Prevents auto-restarting on file changes in production
